b9df81df3d88eb580b44599ed475a87f1ec464cf
rahulholkar16 <usefulltech1819@gmail.com>
2026-06-27 23:38:31 +0530

    Add DB connection in lifespan

--- RepoMindV2/main.py
+++ RepoMindV2/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI;
 from fastapi.middleware.cors import CORSMiddleware;
 from contextlib import asynccontextmanager;
+from config.db import engine;
 
 @asynccontextmanager
 async def lifespan (app: FastAPI):
@@ -9,12 +10,22 @@ async def lifespan (app: FastAPI):
     Here we setup DB connetion.
     and Agent Intialization.
     """
-    yield
+    try:
+        async with engine.connect() as conn:
+            await conn.execute("SELECT 1")
+        print("✅ PostgreSQL Connected")
+    except Exception as e:
+        print(f"❌ Failed to connect to PostgreSQL: {e}")
+        raise
 
+    yield
     print("Application is Stoped");
     """
         We close the DB connection and Agent and Other temp memory.
     """
+
+    await engine.dispose()
+    print("PostgreSQL Connection Pool Closed")
 
 app = FastAPI(lifespan=lifespan);
 
