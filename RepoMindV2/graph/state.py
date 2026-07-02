1b75ce180165851f6b1f3f3412eaaa4008a3095f
rahulholkar16 <usefulltech1819@gmail.com>
2026-07-02 22:00:34 +0530

    Define States of Agent

---
+++ RepoMindV2/graph/state.py
@@ -0,0 +1,11 @@
+from typing import TypedDict, Annotated;
+from langchain_core.messages import BaseMessage;
+from langgraph.graph.message import add_messages;
+
+class State (TypedDict):
+    message: Annotated[list[BaseMessage], add_messages]
+    repo_url: str
+    repo_full_name: str
+    repo_id: str
+    thread_id: str
+
