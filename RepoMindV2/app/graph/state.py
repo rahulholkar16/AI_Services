718c4e5c5c1a0e58e0e90f7c313dbf75aac2dd13
rahulholkar16 <usefulltech1819@gmail.com>
2026-07-05 20:49:07 +0530

    Add Agent State

---
+++ RepoMindV2/app/graph/state.py
@@ -0,0 +1,12 @@
+from typing import TypedDict, Annotated;
+from langchain_core.messages import BaseMessage;
+from langgraph.graph.message import add_messages;
+
+class State (TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+    repo_url: str
+    repo_full_name: str
+    repo_id: str
+    thread_id: str
+    pr_pending: dict | None
+
