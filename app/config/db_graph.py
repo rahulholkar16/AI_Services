dd862326f5ad0cefd0dd09de7ec0796b93d72fe0
rahulholkar16 <usefulltech1819@gmail.com>
2026-07-11 17:53:57 +0530

    Chnage DB URI

--- app/config/db_graph.py
+++ app/config/db_graph.py
@@ -4,7 +4,7 @@ from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver;
 from app.graph import build_graph;
 import app.state as state
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
+DATABASE_URL = os.getenv("CHECKPOINT_DATABASE_URL", "")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1);
 
