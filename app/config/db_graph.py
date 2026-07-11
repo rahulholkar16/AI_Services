055323d9b4b0fda3ce7cddb6ad9eceb21e965029
rahulholkar16 <usefulltech1819@gmail.com>
2026-07-11 17:32:12 +0530

    comment db code

--- app/config/db_graph.py
+++ app/config/db_graph.py
@@ -1,18 +1,18 @@
-import os;
-from contextlib import asynccontextmanager;
-from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver;
-from app.graph import build_graph;
-import app.state as state
+# import os;
+# from contextlib import asynccontextmanager;
+# from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver;
+# from app.graph import build_graph;
+# import app.state as state
 
-DATABASE_URL = os.getenv("DATABASE_URL", "")
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1);
+# DATABASE_URL = os.getenv("DATABASE_URL", "")
+# if DATABASE_URL.startswith("postgres://"):
+#     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1);
 
-@asynccontextmanager
-async def init_agent():
-    async with AsyncPostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:
-        await checkpointer.setup();
-        state.agent = await build_graph(checkpointer);
-        print("✅ Agent ready with Postgres memory!");
-        yield
-    print("Agent connection closed.");
+# @asynccontextmanager
+# async def init_agent():
+#     async with AsyncPostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:
+#         await checkpointer.setup();
+#         state.agent = await build_graph(checkpointer);
+#         print("✅ Agent ready with Postgres memory!");
+#         yield
+#     print("Agent connection closed.");
