c682b8921ae92ed02ad1f28c63fdc26021580a7c
rahulholkar16 <usefulltech1819@gmail.com>
2026-07-17 23:38:35 +0530

    Mograte from Groq to Gemini model

--- app/llm.py
+++ app/llm.py
@@ -1,12 +1,21 @@
 import os;
 from dotenv import load_dotenv;
-from langchain_groq import ChatGroq;
+# from langchain_groq import ChatGroq;
+from langchain.chat_models import init_chat_model
 
 load_dotenv();
 
-llm = ChatGroq(
-    model="openai/gpt-oss-120b",
+llm = init_chat_model(
+    model="gemini-3.1-flash-lite",
+    model_provider="google_genai",   
     temperature=0,
     max_tokens=3000,
-    api_key=os.getenv("GROQ_API_KEY")  
-);
+    api_key=os.getenv("GEMINI_API_KEY"),
+)
+
+# llm = ChatGroq(
+#     model="openai/gpt-oss-120b",
+#     temperature=0,
+#     max_tokens=3000,
+#     api_key=os.getenv("GROQ_API_KEY")  
+# );
