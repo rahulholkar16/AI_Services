8bfd9c124acae48577183d8612f9a8bb66fe04ab
rahulholkar16 <usefulltech1819@gmail.com>
2026-08-03 00:57:41 +0530

    make a pool connection insted of single connection

--- app/config/db_graph.py
+++ app/config/db_graph.py
@@ -1,5 +1,6 @@
 import os;
 from contextlib import asynccontextmanager;
+from psycopg_pool import AsyncConnectionPool;
 from langgraph.checkpoint.postgres.aio import AsyncPostgresSaver;
 from app.graph import build_graph;
 import app.state as state
@@ -8,11 +9,22 @@ DATABASE_URL = os.getenv("CHECKPOINT_DATABASE_URL", "")
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1);
 
+
+connection_kwargs = {
+    "autocommit": True,
+    "prepare_threshold": 0,
+}
+
 @asynccontextmanager
 async def init_agent():
-    async with AsyncPostgresSaver.from_conn_string(DATABASE_URL) as checkpointer:
+    async with AsyncConnectionPool(
+        conninfo=DATABASE_URL,
+        max_size=10,
+        kwargs=connection_kwargs,
+    ) as pool:
+        checkpointer = AsyncPostgresSaver(pool)
         await checkpointer.setup();
         state.agent = await build_graph(checkpointer);
-        print("✅ Agent ready with Postgres memory!");
+        print("✅ Agent ready with Postgres memory (pooled)!");
         yield
     print("Agent connection closed.");
